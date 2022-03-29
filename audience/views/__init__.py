64d46a704c61035b1c5d6a3ade15ae26c9e96dae
Eduard James Aban <arceduardvincent@gmail.com>
2022-03-30 04:06:18 +0800

    fix merged conflict

--- audience/views/__init__.py
+++ audience/views/__init__.py
@@ -1,16 +1,12 @@
 from .dashboard import dashboard_view
 from .course import (
-<<<<<<< HEAD
     CourseContentView,
     CreateCourseView,
     LabTrackView,
     ModifyCourseView,
     SubscriberGradeBookList
-=======
-    CourseContentView, CreateCourseView, LabTrackView, ModifyCourseView,
 )
 
-from .policy import(
+from .policy import (
     PrivacyPolicyView, TermsUseView
->>>>>>> 267834ecf8efeb4bbbbad9643359cb3ed2035ef0
 )
