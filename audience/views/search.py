8d60046c89a6fed9475e7eebb2d6dddfd91f9143
Eduard James Aban <arceduardvincent@gmail.com>
2022-06-09 21:02:00 +0800

    update home and search page

--- audience/views/search.py
+++ audience/views/search.py
@@ -1,10 +1,9 @@
 from django.views.generic import TemplateView, ListView, View
 from django.urls import reverse
 from django.http import HttpResponse
-from lab.models import Course
 from django.shortcuts import render
 
-from lab.models import Course
+from lab.models import Course, Category
 
 
 class SearchView(View):
@@ -12,9 +11,13 @@ class SearchView(View):
 
     def get(self, request, *args, **kwargs):
         search = request.GET.get('search')
-        result = Course.objects.filter(title=search)
+        result = Course.objects.filter(title__icontains=search)
+        category = Category.objects.get(id=2)
+        courses = Course.objects.filter(category=category)
         context = {
             'result': result,
-            'search': search
+            'search': search,
+            'courses': courses,
+            'category': category
         }
         return render(self.request, self.template_name, context)
